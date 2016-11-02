ee7e7dd2560a8486e6f8435674366ac9d812fea6
Stewart Park <hello@stewartjpark.com>
2016-11-02 15:48:14 +0900

    Add vue-mode

--- init.el
+++ init.el
@@ -16,7 +16,7 @@
     better-defaults multiple-cursors
     python-mode elpy py-isort py-autopep8 pyenv-mode-auto
     ruby-mode rspec-mode rbenv rubocop
-    racket-mode elm-mode rust-mode
+    racket-mode elm-mode rust-mode vue-mode
     markdown-mode yaml-mode haskell-mode antlr-mode
     dockerfile-mode nasm-mode go-mode foreman-mode js3-mode json-mode
     scss-mode web-mode rainbow-mode rainbow-delimiters
