03bf4dc905186b83e9fc1b85d084e383e389da99
Stewart Park <hello@stewartjpark.com>
2017-02-11 22:05:04 -0800

    Remove the hightlight setup

--- init.el
+++ init.el
@@ -199,7 +199,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(highlight-indentation-face ((t (:background "#F0F0F0"))))
  '(linum ((t (:height 120 :foreground "#C0C0C0"))))
  '(neo-dir-link-face ((t (:foreground "light green"))))
  '(rainbow-delimiters-depth-1-face ((t (:foreground "grey"))))
