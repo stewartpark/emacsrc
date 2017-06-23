41ab032b238e8d348a3c06d3b996e818f22af3dd
Stewart Park <hello@stewartjpark.com>
2017-06-23 14:18:05 -0700

    Fix some options

--- init.el
+++ init.el
@@ -16,7 +16,7 @@
     better-defaults multiple-cursors
     python-mode elpy py-isort py-autopep8 pyenv-mode-auto
     ruby-mode rspec-mode rbenv rubocop
-    racket-mode elm-mode rust-mode vue-mode
+    racket-mode elm-mode rust-mode vue-mode rjsx-mode
     markdown-mode yaml-mode haskell-mode antlr-mode groovy-mode
     dockerfile-mode nasm-mode go-mode foreman-mode js3-mode json-mode
     scss-mode web-mode rainbow-mode rainbow-delimiters
@@ -137,7 +137,7 @@
 
 ;; fiplr
 (setq fiplr-ignored-globs
-      '((directories (".git" ".svn" ".hg" ".bzr" ".bundle" "__pycache__"))
+      '((directories (".git" ".svn" ".hg" ".bzr" ".bundle" "__pycache__" "node_modules" "bower_modules"))
         (files (".DS_Store" "*.pyc" ".#*" "*~" "*.so" "*.jpg" "*.png" "*.gif" "*.pdf" "*.gz" "*.zip"))))
 
 ;; Line number font size fix
@@ -275,6 +275,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(js2-basic-offset 2)
+ '(js3-consistent-level-indent-inner-bracket t)
  '(js3-auto-indent-p t)
  '(js3-curly-indent-offset 0)
  '(js3-enter-indents-newline t)
