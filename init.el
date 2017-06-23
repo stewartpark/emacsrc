a5a9a74baad8c0b2d367b98e71ae37eab9a24da5
Stewart Park <hello@stewartjpark.com>
2017-06-23 14:21:46 -0700

    Use two whitespaces as indent for js

--- init.el
+++ init.el
@@ -275,7 +275,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(js2-basic-offset 2)
+ '(js-indent-level 2)
  '(js3-consistent-level-indent-inner-bracket t)
  '(js3-auto-indent-p t)
  '(js3-curly-indent-offset 0)
