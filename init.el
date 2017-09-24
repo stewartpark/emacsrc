2267d81a4e9aa7156cc318981a589482e0fe0a85
Stewart Park <hello@stewartjpark.com>
2017-09-24 07:17:47 -0700

    Set tab width to 2

--- init.el
+++ init.el
@@ -280,6 +280,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(tab-width 2)
+ '(css-indent-offset 2)
+ '(ruby-indent-level 2)
  '(js-indent-level 2)
  '(js3-auto-indent-p t)
  '(js3-consistent-level-indent-inner-bracket t)
