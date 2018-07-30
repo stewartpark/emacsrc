e86b5ebf6c50d968ab4e24fa458c15b30a513a9b
Stewart Park <hello@stewartjpark.com>
2018-07-30 00:09:44 -0700

    Add clear functionality to resque-mode

--- lisp/resque-mode.el
+++ lisp/resque-mode.el
@@ -31,20 +31,47 @@
         (buffer-string)
       (kill-buffer))))
 
+(defun resque--remove-queue (queue-name)
+  "Remove a queue.  QUEUE-NAME: name of the queue."
+  (let ((url-request-method "POST")
+        (url-request-data ""))
+    (with-current-buffer
+        (url-retrieve-synchronously
+         (concat resque-base-url "queues/" queue-name "/remove"))
+      (prog1
+          (buffer-string)
+        (kill-buffer)))))
+
+(defun resque--clear-failed ()
+  "Clear failed tasks."
+  (let ((url-request-method "POST")
+        (url-request-data ""))
+    (with-current-buffer
+        (url-retrieve-synchronously
+         (concat resque-base-url "failed/clear"))
+      (prog1
+          (buffer-string)
+        (kill-buffer)))))
+
+(defun resque--get-queues ()
+  "List queues."
+  (mapcar
+   (lambda (x) (cdr x))
+   (s-match-strings-all
+    "class=\"queue\" href=\"/.+?\">\\(.+?\\)<.+?size'>\\(.+?\\)<"
+    (resque--send-request))))
+
+(defun resque--contains (queue-name)
+  "QUEUE-NAME: queue name to check."
+  (member queue-name (mapcar 'car (resque--get-queues))))
+
 (defun resque--render-string ()
   "Render the string for the buffer."
   (let ((l (make-list 0 nil)))
     (add-to-list 'l "* Resque Monitor\n")
     (add-to-list 'l "|Queue|# of Messages In-flight|\n")
     (add-to-list 'l "|-----+-----------------------|\n")
-    (dolist
-        (queues
-         (mapcar
-          (lambda (x) (cdr x))
-          (s-match-strings-all
-           "class=\"queue\" href=\"/.+?\">\\(.+?\\)<.+?size'>\\(.+?\\)<"
-           (resque--send-request))))
-
+    (dolist (queues (resque--get-queues))
       (add-to-list 'l
                    (format "| %s | %s |\n" (car queues) (car (cdr queues)))))
     (apply 'concat (reverse l))))
@@ -65,6 +92,17 @@
                                (interactive)
                                (resque--render)
                                (message "Resque refreshed.")))
+    (local-set-key (kbd "c") (lambda ()
+                               (interactive)
+                               (if (resque--contains (current-word))
+                                   (when (y-or-n-p (format "Clear %s? " (current-word)))
+                                     (if (string= "failed" (current-word))
+                                         (resque--clear-failed)
+                                       (resque--remove-queue (current-word)))
+                                     (message (format "%s cleared." (current-word)))
+                                     (resque--render))
+                                 (message "Select queue to clear."))))
+
     (resque--render)
     (message "Resque Monitor")))
 
